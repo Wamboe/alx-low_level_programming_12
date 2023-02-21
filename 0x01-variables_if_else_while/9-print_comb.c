b5b48cc68f2accc566795db4b3eba4b262a8623c
Wamboe <sheila.karienye@gmail.com>
2023-02-20 20:27:35 -0800

    posible combos of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printing single digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num == 9)
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
