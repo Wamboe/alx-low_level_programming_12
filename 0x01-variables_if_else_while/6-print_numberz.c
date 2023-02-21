8ff668650ef85724cfbca46947327a546cf84668
Wamboe <sheila.karienye@gmail.com>
2023-02-20 19:45:04 -0800

    using putchar to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printing single digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num = '0';
+
+	for (; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
