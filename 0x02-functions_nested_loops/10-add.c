789f783819329e200b3fb2da0a398a9a7919518a
Wamboe <sheila.karienye@gmail.com>
2023-02-24 13:54:36 -0800

    function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds 2 ints
+ *
+ * @a: first int to be takein
+ * @b: second int to be taken in
+ *
+ * Description: adds 2 ints.
+ *
+ * Return: returns the result of a sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,8 +13,9 @@
 
 
 int _putchar(char c);
-void times_table(void);
-/* void jack_bauer(void);
+int add(int, int);
+/*void times_table(void);
+void jack_bauer(void);
  -int print_last_digit(int);
 int _abs(int);
 int print_sign(int n);
