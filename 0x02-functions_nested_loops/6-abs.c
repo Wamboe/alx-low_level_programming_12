f44ccbf0c9e127fe38402570541c2604c954b487
Wamboe <sheila.karienye@gmail.com>
2023-02-24 17:02:46 -0800

    function that computes the absolute value of an integer error fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,10 +12,27 @@
 
 int _abs(int i)
 {
+	/*int last;
+	int second_last;*/
+
 	if (i < 0)
 	{
 		i = i * -1;
 	}
 
+	/*second_last = (i / 10) % 10;
+	last = i % 10;
+
+	if (second_last == 0)
+	{
+		_putchar(last + '0');
+	}
+	else
+	{
+		_putchar(second_last + '0');
+		_putchar(last + '0');
+	}
+
+	_putchar('\n');*/
 	return (i);
 }
