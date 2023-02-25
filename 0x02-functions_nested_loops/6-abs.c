c417574119d74c2b54f65edf951f71eca8bc07f1
Wamboe <sheila.karienye@gmail.com>
2023-02-24 17:05:34 -0800

    function that computes the absolute value of an integer betty fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,27 +12,10 @@
 
 int _abs(int i)
 {
-	/*int last;
-	int second_last;*/
-
 	if (i < 0)
 	{
 		i = i * -1;
 	}
 
-	/*second_last = (i / 10) % 10;
-	last = i % 10;
-
-	if (second_last == 0)
-	{
-		_putchar(last + '0');
-	}
-	else
-	{
-		_putchar(second_last + '0');
-		_putchar(last + '0');
-	}
-
-	_putchar('\n');*/
 	return (i);
 }
