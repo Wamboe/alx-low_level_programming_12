d2c41843784af23f0c835c40b5edab1115a41fc1
Wamboe <sheila.karienye@gmail.com>
2023-02-27 17:59:47 -0800

    fxn that updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - makes a variable pointer 98
+ *
+ * @n: pointer to the integer declared in fxn calling the below
+ * Description: function that updates the value it points to to 98
+ * Return: n/a
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
