b7a99cb0dea60150bb6b5d00ebdcb42499b056ee
Wamboe <sheila.karienye@gmail.com>
2023-03-02 09:26:14 -0800

    function that capitalizes all words of a string error fix.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,14 @@
 char *cap_string(char *c)
 {
 	char s[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int specialLen = sizeof(special) / sizeof(char);
+	int specialLen = sizeof(s) / sizeof(char);
 	int i, j = 0;
 
 	while (*(c + j) != '\0')
 	{
 		for (i = 0; i < specialLen; i++)
 		{
-			if (*(c + j) == special[i] && (*(c + j + 1) >= 'a' && *(c + j + 1) <= 'z'))
+			if (*(c + j) == s[i] && (*(c + j + 1) >= 'a' && *(c + j + 1) <= 'z'))
 			{
 				*(c + j + 1) = *(c + j + 1) - 32;
 			}
